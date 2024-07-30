2998dba22b60867301bb8bd8b9cf268fcded63bf
Elena Churilova <ellena.churilova@gmail.com>
2024-07-30 12:35:59 -0700

    saves progress

--- main.swift
+++ main.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-// * Create the `Todo` struct.
-// * Ensure it has properties: id (UUID), title (String), and isCompleted (Bool).
 struct Todo : CustomStringConvertible, Codable {
     var id: UUID
     var title: String
@@ -11,26 +9,18 @@ struct Todo : CustomStringConvertible, Codable {
     }
 }
 
-// Create the `Cache` protocol that defines the following method signatures:
-//  `func save(todos: [Todo])`: Persists the given todos.
-//  `func load() -> [Todo]?`: Retrieves and returns the saved todos, or nil if none exist.
 protocol Cache {
     func save(todos: [Todo])
     func load() -> [Todo]?
 }
 
-// `FileSystemCache`: This implementation should utilize the file system 
-// to persist and retrieve the list of todos. 
-// Utilize Swift's `FileManager` to handle file operations.
+
 final class JSONFileManagerCache: Cache {
 
     // Initilize FileManager first, and then create a new directory -> return its URL
     func getFileURL() -> URL {
         let fileManager = FileManager.default
         let currentDirectoryURL = URL(fileURLWithPath: fileManager.currentDirectoryPath)
-
-        // REMOVE
-        print(currentDirectoryURL)
         
         // create UserData folder in the current directory
         let userDataURL = currentDirectoryURL.appendingPathComponent("UserData")
@@ -52,7 +42,6 @@ final class JSONFileManagerCache: Cache {
             let jsonData = try jsonEncoder.encode(todos)
             let fileURL = getFileURL()
             try jsonData.write(to: fileURL)
-            print("Successfully wrote to file!")
         } catch {
             print("JSONFileManagerCache: error writing to file: \(error)")
         }
@@ -73,20 +62,19 @@ final class JSONFileManagerCache: Cache {
 
 }
 
-// `InMemoryCache`: : Keeps todos in an array or similar structure during the session. 
-// This won't retain todos across different app launches, 
-// but serves as a quick in-session cache.
-
 final class InMemoryCache: Cache {
-    // IMPLEMENT LATER, NOT A PRIORITY 
+
+    private var inMemoryTodos = [Todo]()
+
+    func save(todos: [Todo]) {
+        inMemoryTodos = todos
+    }
+
+    func load() -> [Todo]? {
+        return inMemoryTodos
+    }
 }
 
-// The `TodosManager` class should have:
-// * A function `func listTodos()` to display all todos.
-// * A function named `func addTodo(with title: String)` to insert a new todo.
-// * A function named `func toggleCompletion(forTodoAtIndex index: Int)` 
-//   to alter the completion status of a specific todo using its index.
-// * A function named `func deleteTodo(atIndex index: Int)` to remove a todo using its index.
 final class TodoManager {
 
     private let cache: Cache
@@ -131,12 +119,6 @@ final class TodoManager {
 
 }
 
-
-// * The `App` class should have a `func run()` method, this method should perpetually 
-//   await user input and execute commands.
-//  * Implement a `Command` enum to specify user commands. Include cases 
-//    such as `add`, `list`, `toggle`, `delete`, and `exit`.
-//  * The enum should be nested inside the definition of the `App` class
 final class App {
 
     private let todoManager: TodoManager
@@ -147,12 +129,12 @@ final class App {
 
     func run() {
         var iterate = true
-        let welcome = "🌟 Welcome to To Do CLI 🌟"
-        let question = "\nWhat would you like to do? \n(add, list, toggle, delete, exit): "
-        let enterTodoTitle = "Enter To Do Title: "
+        let welcome = "\n🌟 Welcome to To Do CLI 🌟"
+        let question = "\nWhat would you like to do? (add, list, toggle, delete, exit): \n"
+        let enterTodoTitle = "\nEnter To Do Title: "
         let todoAdded = "\n📌 Todo added! "
-        let unknownInput = "I'm not quite sure I know this command 🤔.\nTry again? -> "
-        let kBye = "Okay bye! 👋"
+        let unknownInput = "\n🤔 I'm not quite sure I know this command.\nTry again? -> "
+        let kBye = "\nOkay bye! 👋"
         let yourTodos = "\n📝 Your Todos:\n"
         let enterNumberToToggle = "\nEnter the number of the todo to toggle: "
         let enterNumberToDelete = "\nEnter the number of the todo to delete: "
@@ -187,6 +169,7 @@ final class App {
                     print(enterNumberToDelete)
                     if let todoIndex = readLine(), let index = Int(todoIndex) {
                         todoManager.deleteTodo(atIndex: index - 1)
+                        prettyListTodos(yourTodos)
                     } else {
                         print(unknownInput)
                     }
@@ -220,6 +203,5 @@ final class App {
 }
 
 
-// TODO: Write code to set up and run the app.
-let app = App(cache: JSONFileManagerCache())
+let app = App(cache: JSONFileManagerCache()) // can change to InMemoryCache()
 app.run()
